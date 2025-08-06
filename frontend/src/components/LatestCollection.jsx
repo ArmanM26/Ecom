7f0c9ee9dd23849456c23ff69188f7af7ef86ad7
ArmanM26 <armanmkrtchyan260612@gmail.com>
2025-08-06 15:40:28 +0400

    Update LatestCollection to use last 10 products

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,16 +1,40 @@
-import React,{ lazy } from 'react'
+import React,{ lazy, useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
-
+import Title from './Title'
+import ProductItem from './ProductItem'
 const LatestCollection = () => {
 
     const {products} = useContext(ShopContext)
+    const [latestProducts,setlatestProducts] = useState ([]) 
 
+    useEffect(() => {
+        setlatestProducts(products.slice(0,10))
+    },[])
 
-    console.log(products)
-
+    
   return (
-    <div></div>
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={"Latest"} text2={"Collection"}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde enim odit facilis! At magnam dolor inventore in sed ipsam officia, saepe consequuntur, adipisci ipsum debitis vero blanditiis nihil modi earum.
+            </p>
+
+{/* {Rendering Products} */}
+
+<div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 '>
+    {
+        latestProducts.map((item,index)=>(
+            <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price} />
+        )
+        
+        )
+    }
+</div>
+
+        </div>
+    </div>
   )
 }
 
